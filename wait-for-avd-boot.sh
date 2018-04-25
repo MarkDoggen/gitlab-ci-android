bfd7afd1e4a69d40091f02532a19f0c633ff42c2
Mark Doggen <markdoggen@gmail.com>
2018-04-25 11:52:05 +0200

    Use property dev.bootcomplete

--- wait-for-avd-boot.sh
+++ wait-for-avd-boot.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
  
-OUT=`/sdk/platform-tools/adb shell getprop sys.boot_completed`
+OUT=`/sdk/platform-tools/adb shell getprop dev.bootcomplete`
 RES="1"
 
 while [[ ${OUT:0:1}  != $RES ]]; do
-		OUT=`/sdk/platform-tools/adb shell getprop sys.boot_completed`
+		OUT=`/sdk/platform-tools/adb shell getprop dev.bootcomplete`
 		echo 'Waiting for emulator to fully boot...'
+		echo $OUT
 		sleep 5
 done
 
